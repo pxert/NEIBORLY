3bb2d9db7aacafd2653065fa62a335a68bad136a
salmaols <salma.oulous@isfsc.be>
2024-12-18 12:36:52 +0100

    responsive header

---
+++ secondemain.php
@@ -0,0 +1,159 @@
+<?
+/* template Name : secondemain */
+?>
+
+<?php get_header(); ?>
+<?php get_footer(); ?>
+
+
+
+    <div>
+        <h1 class="text-center mb-5" >Donnez une Nouvelle Vie à Vos Objets ?</h1>  
+        <button type="button" class="btn align-items-end rounded-pill">Envoyer un message</button>
+    </div>
+
+
+
+      <div class="row row-cols-1 row-cols-md-3 g-4 m-5">
+        <div class="col">
+          <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col">
+          <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a short card.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col">
+            <div class="card h-100">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col">
+            <div class="card h-100">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card h-100">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a short card.</p>
+              </div>
+            </div>
+          </div>
+  
+          <div class="col">
+            <div class="card h-100">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              </div>
+            </div>
+          </div>
+  
+  
+          <div class="col">
+              <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+              </div>
+            </div>
+
+
+        <div class="col">
+          <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+<!-- footer  -->
+<footer class="row text-center row-cols-1 row-cols-sm-2 row-cols-md-5 p-5 border-top mx-0">
+    <div class="col mb-3 mx-4">
+      <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+        <img src="Frame 122.svg" alt="">
+      </a>
+      <p class="text-body-secondary">© 2024</p>
+    </div>
+  
+  
+    <div class="col mb-3 mx-4">
+      <h5>ressources</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">politique de confidalité</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">conditions générales</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">mentions légales</a></li>
+      </ul>
+    </div>
+  
+    <div class="col mb-3 mx-4">
+      <h5>contactez-nous</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">contact</a></li>
+      </ul>
+    </div>
+  
+    <div class="col mb-3 mx-4">
+      <h5>suivez-nous sur</h5>
+      <div class="d-flex align-items-center justify-content-center">
+      <ul class="nav">
+        <li class="nav-item mx-1"><a href="https://instagram.com" target="_blank">
+            <img src="icone/VectorIG.svg" alt="Instagram" height="50" width="50""></a></li>
+        <li class="nav-item mx-1"><a href="https://facebook.com" target="_blank">
+            <img src="icone/VectorFB.svg" alt="Facebook" height="50" width="50" /></a></li>
+        <li class="nav-item mx-1"><a href="https://twitter.com" target="_blank">
+            <img src="icone/VectorX.svg" alt="Twitter" height="50" width="50" /></a></li>
+      </ul>
+    </div>
+    
+    </div>
+  
+  
+  </footer>
